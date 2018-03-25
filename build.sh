fd6105d66a026d5fa7659077cf207634b79c6916
Brian Ballsun-Stanton <brian@fedarch.org>
2018-03-26 10:04:44 +1100

    photos

--- build.sh
+++ build.sh
@@ -3,9 +3,21 @@
 
 #source /opt/context/tex/setuptex
 
+
+if [ -n "$1" ]; then
+	compile=$1
+else
+	compile="UserToDev.tex"
+fi
+
+
+base=$(basename -s ".tex" $compile)
 rm -f *.log
 rm -f *.pdf
 rm -f *.tuc
 
-context --batchmode --silent $1 > run.log
+context --batchmode --silent $compile > run.log
 grep -A 15 	"tex error" run.log 
+
+
+evince ${base}.pdf
